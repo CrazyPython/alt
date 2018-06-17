bf17e9f87f8e14b07add4d92de1f24ba1f604ac0
James <Jamtlu@gmail.com>
2018-06-17 10:51:40 -0400

    Change AltJS Module to "alt/types" module
    It's a hack to get JetBrains WebStorm to recognize the methods on my own classes. 
    
    `class FooStore implements AltJS.StoreModel {` is invalid syntax to the React Native bundler, but `class FooStore implements StoreModel` us not. The first works with WebStorm natively, the second does not.

--- typings/alt/alt.d.ts
+++ typings/alt/alt.d.ts
@@ -6,7 +6,7 @@
 ///<reference path="../react/react.d.ts"/>
 ///<reference path="../es6-promise/es6-promise.d.ts" />
 
-declare module AltJS {
+declare module "alt/types" {
 
   interface StoreReduce {
     action:any;
